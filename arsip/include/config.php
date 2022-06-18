ca7129ac66e063558bf1f9474e7e6ab392f74b90
Weike Sandy Putra Matanari <83500399+coegsekalianda@users.noreply.github.com>
2022-06-18 18:09:40 +0700

    Update config.php

--- arsip/include/config.php
+++ arsip/include/config.php
@@ -1,5 +1,5 @@
 <?php
-$host     = "localhost";    // Nama host
-$username = "root";         // Username database
-$password = "";   // Password database
-$database = "arsip";   // Nama database
+$host     = "sql111.epizy.com";    // Nama host
+$username = "epiz_31981466";         // Username database
+$password = "Ossr7OJqfeCM";   // Password database
+$database = "epiz_31981466_arsip";   // Nama database
