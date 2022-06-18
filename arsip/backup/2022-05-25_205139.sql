c19a9f35625ffba27b7a2b91fd348b10e5426627
Weike Sandy Putra Matanari <83500399+coegsekalianda@users.noreply.github.com>
2022-06-18 18:00:30 +0700

    Add files via upload

---
+++ arsip/backup/2022-05-25_205139.sql
@@ -0,0 +1,133 @@
+DROP TABLE tbl_disposisi;
+
+CREATE TABLE `tbl_disposisi` (
+  `id_disposisi` int(10) NOT NULL AUTO_INCREMENT,
+  `tujuan` varchar(250) NOT NULL,
+  `isi_disposisi` mediumtext NOT NULL,
+  `sifat` varchar(100) NOT NULL,
+  `batas_waktu` date NOT NULL,
+  `catatan` varchar(250) NOT NULL,
+  `id_surat` int(10) NOT NULL,
+  `id_user` tinyint(2) NOT NULL,
+  PRIMARY KEY (`id_disposisi`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+INSERT INTO tbl_disposisi VALUES("1","gadak","kosoong","Rahasia","2022-05-25","0","1","1");
+INSERT INTO tbl_disposisi VALUES("2","segera","segera kerjakan","Penting","2022-05-26","segera","3","1");
+
+
+
+DROP TABLE tbl_instansi;
+
+CREATE TABLE `tbl_instansi` (
+  `id_instansi` tinyint(1) NOT NULL,
+  `institusi` varchar(150) NOT NULL,
+  `nama` varchar(150) NOT NULL,
+  `status` varchar(150) NOT NULL,
+  `alamat` varchar(150) NOT NULL,
+  `kepsek` varchar(50) NOT NULL,
+  `nip` varchar(25) NOT NULL,
+  `website` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `logo` varchar(250) NOT NULL,
+  `id_user` tinyint(2) NOT NULL,
+  PRIMARY KEY (`id_instansi`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO tbl_instansi VALUES("1","Universitas Lampung","Kantor Perkasa","Aktif","Jl. Prof. Dr. Ir. Sumantri Brojonegoro No.1, Gedong Meneng, Kec. Rajabasa, Kota Bandar Lampung, Lampung 35141","Weike Sandy Putra Matanari","2017051065","https://github.com/coegsekalianda?tab=repositories","dotariki45@gmail.com","1200px-Mail-closed.svg.png","1");
+
+
+
+DROP TABLE tbl_klasifikasi;
+
+CREATE TABLE `tbl_klasifikasi` (
+  `id_klasifikasi` int(5) NOT NULL AUTO_INCREMENT,
+  `kode` varchar(30) NOT NULL,
+  `nama` varchar(250) NOT NULL,
+  `uraian` mediumtext NOT NULL,
+  `id_user` tinyint(2) NOT NULL,
+  PRIMARY KEY (`id_klasifikasi`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+INSERT INTO tbl_klasifikasi VALUES("1","01","Pemberitahuan","Surat Pemberitahuan","1");
+INSERT INTO tbl_klasifikasi VALUES("2","02","Pengajuan","Surat Pengajuan","1");
+INSERT INTO tbl_klasifikasi VALUES("3","03","pencetakan","gunanya untuk print","1");
+
+
+
+DROP TABLE tbl_sett;
+
+CREATE TABLE `tbl_sett` (
+  `id_sett` tinyint(1) NOT NULL,
+  `surat_masuk` tinyint(2) NOT NULL,
+  `surat_keluar` tinyint(2) NOT NULL,
+  `referensi` tinyint(2) NOT NULL,
+  `id_user` tinyint(2) NOT NULL,
+  PRIMARY KEY (`id_sett`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO tbl_sett VALUES("1","10","10","10","1");
+
+
+
+DROP TABLE tbl_surat_keluar;
+
+CREATE TABLE `tbl_surat_keluar` (
+  `id_surat` int(10) NOT NULL AUTO_INCREMENT,
+  `no_agenda` int(10) NOT NULL,
+  `tujuan` varchar(250) NOT NULL,
+  `no_surat` varchar(50) NOT NULL,
+  `isi` mediumtext NOT NULL,
+  `kode` varchar(30) NOT NULL,
+  `tgl_surat` date NOT NULL,
+  `tgl_catat` date NOT NULL,
+  `file` varchar(250) NOT NULL,
+  `keterangan` varchar(250) NOT NULL,
+  `id_user` tinyint(2) NOT NULL,
+  PRIMARY KEY (`id_surat`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+INSERT INTO tbl_surat_keluar VALUES("2","1","unila","abc","test","abc","2022-05-26","2022-05-25","4943-Screenshot_1.png","testing","1");
+
+
+
+DROP TABLE tbl_surat_masuk;
+
+CREATE TABLE `tbl_surat_masuk` (
+  `id_surat` int(10) NOT NULL AUTO_INCREMENT,
+  `no_agenda` int(10) NOT NULL,
+  `no_surat` varchar(50) NOT NULL,
+  `asal_surat` varchar(250) NOT NULL,
+  `isi` mediumtext NOT NULL,
+  `kode` varchar(30) NOT NULL,
+  `indeks` varchar(30) NOT NULL,
+  `tgl_surat` date NOT NULL,
+  `tgl_diterima` date NOT NULL,
+  `file` varchar(250) NOT NULL,
+  `keterangan` varchar(250) NOT NULL,
+  `id_user` tinyint(2) NOT NULL,
+  PRIMARY KEY (`id_surat`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+INSERT INTO tbl_surat_masuk VALUES("1","1","2019/IX/S-12","CV. Air Mail","lorem","01","132","2019-11-01","2019-11-22","5271-Surat Edaran.pdf","Edaran","1");
+INSERT INTO tbl_surat_masuk VALUES("3","2","2","unila","test","abc","abc","2022-05-25","2022-05-25","2023-Kelompok ADSI SKPL Surat Menyurat.docx","kosong","1");
+
+
+
+DROP TABLE tbl_user;
+
+CREATE TABLE `tbl_user` (
+  `id_user` tinyint(2) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(35) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `nip` varchar(25) NOT NULL,
+  `admin` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+INSERT INTO tbl_user VALUES("1","admin","21232f297a57a5a743894a0e4a801fc3","Administrator","2017051065","1");
+INSERT INTO tbl_user VALUES("2","pengguna","8b097b8a86f9d6a991357d40d3d58634","Username","58647333","3");
+
+
+
